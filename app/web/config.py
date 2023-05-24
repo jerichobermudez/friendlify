5a8691267aaffa0ef9bd232132871dff00c60036
jerichobermudez <jerichoramosbermudez@gmail.com>
2023-05-24 12:56:43 +0800

    Add config.py

---
+++ app/web/config.py
@@ -0,0 +1,12 @@
+ACTIVATION_EXPIRATION=3600 # 3600milliseconds = 1hr
+POST_PER_PAGE=10
+COMMENTS_PER_POST=5
+COMMON_PASSWORDS=[
+    'Pa$$w0rd',
+    'p@ssw0rd',
+    'p@ssword',
+    '11223344',
+    'password',
+    '123456',
+    'qwerty'
+]
